5de0fb3c775bb9226426dfb7ce8832544630bf97
tsun-code <61623464+tsun-code@users.noreply.github.com>
2020-04-14 01:12:33 +0800

    Add original sources and final config

---
+++ wildrydes-terraform/resources/s3/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-southeast-1_5bPwqH4fG', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2m0d1k3alrvjjk16b6tj9s5bae', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://jvhpnln3mb.execute-api.ap-southeast-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
